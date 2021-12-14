d1acd7b8e129b96ce32fe8d1e68680be230323a7
Jayallstars <jayza0072555@gmail.com>
2021-12-14 11:36:48 +0700

    edit

--- calculator.js
+++ calculator.js
@@ -2,20 +2,32 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req, res) {
-  var num1 = Number(req.body.num1);
-  var num2 = Number(req.body.num2);
-  var result = num1 + num2;
-  res.send("This is your number " + result);
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    res.send("This is your number " + result);
+});
+
+app.get("/bmicalculator", function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res) {
+    var num1 = Number(req.body.weight);
+    var num2 = Number(req.body.height);
+    var result = num1 + num2;
+    res.send("Your BMI is " + result);
 });
 
 
+
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+    console.log("Server started on port 3000");
 });
